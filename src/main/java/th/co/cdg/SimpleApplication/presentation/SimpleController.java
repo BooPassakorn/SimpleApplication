11490523175fd73a061e4b7f926a43aee5995fba
BooPassakorn <t.passakorn4999@gmail.com>
2025-02-27 16:40:09 +0700

    @GetMapping

--- src/main/java/th/co/cdg/SimpleApplication/presentation/SimpleController.java
+++ src/main/java/th/co/cdg/SimpleApplication/presentation/SimpleController.java
@@ -2,33 +2,31 @@ package th.co.cdg.SimpleApplication.presentation;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
 
 @RestController
 public class SimpleController {
 
     // ---- #1 HTTP Methods ---- //
     // 1. @GetMapping
-    // 2. @PostMapping
-    // 3. @PutMapping
-    // 4. @DeleteMapping
 
     // ---- #2 Base API Name ---- //
-    // Ex. /hello-world
+    // /hello-with-your-name
 
     // ---- #3 Request Param ---- //
-    // Ex. ?name=angel
+    // ?name=boo
 
     // ---- #4 Response HTTP Status ---- //
     // 200 OK
 
     // ---- #5 Response Body ---- //
-    // Hello (name) !!!.
+    // Hello boo !!!
 
-    @GetMapping(value = "hello-with-your-name/{name}")
-    public ResponseEntity<String> getHelloWorld(@PathVariable(name = "name") String name) { //required = false จะส่งก็ได้ไม่ส่งก็ได้ ถ้าเป็น true ถ้าไม่ส่งจะเกิดเคส 400
+    @GetMapping(value = "hello-with-your-name") // #1 // #2
+    public ResponseEntity<String> getHelloWorld(@RequestParam(name = "name") String name) { // #3 //required = false จะส่งก็ได้ไม่ส่งก็ได้ ถ้าเป็น true ถ้าไม่ส่งจะเกิดเคส 400
         return ResponseEntity
-                .ok()
-                .body("Hello " + name + " !!!");
+                .ok() // #4
+                .body("Hello " + name + " !!!"); // #5
     }
 
 }
