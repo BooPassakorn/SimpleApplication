be90d27cf985af0d2fdcf4a065f721239deffe3a
BooPassakorn <t.passakorn4999@gmail.com>
2025-03-04 13:35:46 +0700

    add @PersistenceContext and EntityManager

--- src/main/java/th/co/cdg/SimpleApplication/repository/UserRepository.java
+++ src/main/java/th/co/cdg/SimpleApplication/repository/UserRepository.java
@@ -1,9 +1,14 @@
 package th.co.cdg.SimpleApplication.repository;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
 }
 
 
