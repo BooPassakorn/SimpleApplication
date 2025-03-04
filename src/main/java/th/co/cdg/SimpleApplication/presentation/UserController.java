a28e3c73baa7a62c3dd990353854ce0808e64a8a
BooPassakorn <t.passakorn4999@gmail.com>
2025-03-04 13:53:20 +0700

    Create UserController file

---
+++ src/main/java/th/co/cdg/SimpleApplication/presentation/UserController.java
@@ -0,0 +1,14 @@
+package th.co.cdg.SimpleApplication.presentation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import th.co.cdg.SimpleApplication.repository.UserRepository;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+}
